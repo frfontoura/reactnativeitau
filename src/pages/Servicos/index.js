4da2491dd76b28bde05adaca0ddb30c3f797f1ce
frfontoura <frfontoura@gmail.com>
2019-06-17 18:39:04 -0300

    Criação da tela Servicos

---
+++ src/pages/Servicos/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// import { Container } from './styles';
+
+export default function Servicos() {
+  return (
+    <View>
+      <Text>Serviços</Text>
+    </View>
+  );
+}
