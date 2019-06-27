d53adcbc49b99e6fd29a0ad216bda0e15d0f1813
frfontoura <frfontoura@gmail.com>
2019-06-27 20:55:37 -0300

    Removendo warning para o ViewPagerAndroid

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
-
 import '~/config/ReactotronConfig';
-
 import { Provider } from 'react-redux';
 import store from './store';
+import { YellowBox } from 'react-native';
 
 import Header from '~/components/Header';
 import Routes from '~/routes';
+
+YellowBox.ignoreWarnings(['ViewPagerAndroid']);
 
 const App = () => (
   <Provider store={store}>
